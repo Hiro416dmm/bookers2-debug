750669f1465f903d8d0cdcd3649520897f4fa538
Hiroki Yoshida <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-02-08 08:37:37 +0000

    kadai4

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,8 +1,7 @@
 class BookComment < ApplicationRecord
-  class BookComment < ApplicationRecord
+
     belongs_to :user
     belongs_to :book
-    
+
     validates :comment, presence: true
-  end
 end
