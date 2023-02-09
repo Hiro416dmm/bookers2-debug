4db684b7ecfeae8d9520c7df1808c95a53f42dd1
Hiroki Yoshida <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-02-09 07:58:23 +0000

    kadai5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "search" => "searches#search"
+  get '/search', to: 'searches#search'
   
   root to: "homes#top"
   get "homes/about"
