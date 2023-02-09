96f09a43b6f0c818c4f15f8a7857e5d02d8f178b
Hiroki Yoshida <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-02-09 06:39:09 +0000

    kadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
-  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   # フォローをした、されたの関係
-  has_many :relationship, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 一覧画面で使う
   has_many :followings, through: :relationships, source: :followed
@@ -26,12 +25,12 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
     # フォローしたときの処理
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
   # フォローを外すときの処理
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
   # フォローしているか判定
   def following?(user)
